9d6080b910ecf6841a3123742e25cb6a5f989db8
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2021-02-23 12:32:27 +0600

    update main.js file in vue project

--- shop/src/main.js
+++ shop/src/main.js
@@ -4,19 +4,13 @@ import router from "./router"
 import store from "./store"
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { sync } from 'vuex-router-sync'
-
-sync(store, router)
-
-
+const unsync = sync(store, router)
 Vue.use(IconsPlugin);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
-
-import '../public/assets/scss/app.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-
+unsync()
 new Vue({
   router,
   store,
