67a9e2ed3be7ff57cd88e7104c7cf1d724b6b4e3
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2021-02-23 17:34:11 +0600

    improve some code and some text

--- shop/src/main.js
+++ shop/src/main.js
@@ -4,13 +4,14 @@ import router from "./router"
 import store from "./store"
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { sync } from 'vuex-router-sync'
-const unsync = sync(store, router)
+sync(store, router)
 Vue.use(IconsPlugin);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-unsync()
+
 new Vue({
   router,
   store,
