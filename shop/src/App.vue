ecc7a50153ae260b3a8f3316a45ae0dab8ee0f30
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2021-02-26 16:05:45 +0600

    add app.vue style scoped text

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -8,7 +8,7 @@
 export default {};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
